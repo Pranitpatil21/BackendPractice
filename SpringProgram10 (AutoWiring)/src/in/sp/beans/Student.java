c94c02263103412b2ca43bf828c457050081c6bb
Pranit Patil <Pranitpatil8337@gmail.com>
2026-02-09 01:49:26 +0530

    Backend

---
+++ SpringProgram10 (AutoWiring)/src/in/sp/beans/Student.java
@@ -0,0 +1,42 @@
+package in.sp.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Student {
+
+	private String name;
+	private int rollno;
+
+	@Autowired // By Using This we dont need to maunually write dependeny injection in config
+				// file
+	private Address address;
+
+	@Autowired              //our bean object in config file will automatically injected in this 
+	private Subjects subjects;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+
+//	public void setSubjects(Subjects subjects) {
+//		this.subjects = subjects;
+//	}
+
+	
+//		public void setAddress(Address address) {
+//			this.address = address;                           //no need of manualy DI we have use autowired
+//		}
+
+	
+	public void display() {
+		System.out.println("Name: " + name);
+		System.out.println("rollNo: " + rollno);
+		System.out.println("Address: " + address);
+		System.out.println("Subjects: " + subjects);
+	}
+}
