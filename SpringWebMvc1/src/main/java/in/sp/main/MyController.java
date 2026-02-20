6db2fa8eda757a7af89aa055ba2791c919cfbee0
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-02-20 23:41:05 +0530

    MyController.java

--- SpringWebMvc1/src/main/java/in/sp/main/MyController.java
+++ SpringWebMvc1/src/main/java/in/sp/main/MyController.java
@@ -1,7 +1,7 @@
 package in.sp.main;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
