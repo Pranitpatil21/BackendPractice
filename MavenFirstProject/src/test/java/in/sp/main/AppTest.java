285856efe979db4ea1cc54966b27e3db64ab4ead
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-02-28 21:16:20 +0530

    AppTest.java

--- MavenFirstProject/src/test/java/in/sp/main/AppTest.java
+++ MavenFirstProject/src/test/java/in/sp/main/AppTest.java
@@ -1,6 +1,6 @@
 package in.sp.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue; 
 
 import org.junit.jupiter.api.Test;
 
